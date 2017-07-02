01fe76fc18c23f9de35651cef89a4edd7a53625d
Murilo Fidelis <murilofidelis2012@gmail.com>
2017-07-02 19:09:57 -0300

    implementando carrinho de compras

---
+++ src/app/restaurant-detail/shopping-cart/cart-item.model.ts
@@ -0,0 +1,10 @@
+import { MenuItem } from './../menu-item/menu-item.model';
+
+export class CartItem {
+
+    constructor(public menuItem: MenuItem, public quantidade: number = 1) { }
+
+    calcularValor(): number {
+        return this.menuItem.price * this.quantidade
+    }
+}
---
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -0,0 +1,30 @@
+import { MenuItem } from './../menu-item/menu-item.model';
+import { CartItem } from './cart-item.model';
+export class ShoppingCartService {
+
+    items: CartItem[] =[]
+
+    limpar() {
+        this.items = []
+        }
+
+    adicionarItens(item: MenuItem) {
+        let foundItem = this.items.find((mItem) => mItem.menuItem.id === item.id)
+        if (foundItem) {
+            foundItem.quantidade = foundItem.quantidade + 1
+        } else {
+            this.items.push(new CartItem(item))
+        }
+    }
+
+    removerItem(item: CartItem) {
+        this.items.splice(this.items.indexOf(item), -1)
+    }
+
+    total(): number {
+        return this.items
+        .map(item => item.calcularValor())
+        .reduce((prev, value) => prev + value, 0)
+    }
+
+}
