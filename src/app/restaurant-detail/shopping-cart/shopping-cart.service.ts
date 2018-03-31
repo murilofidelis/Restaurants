6dba74b7cc3f67da2166f1123bab5de01ed4f98d
Murilo Fidelis <murilofidelis2012@gmail.com>
2018-03-31 10:48:51 -0300

    alterando  notificação

--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -1,40 +1,50 @@
+import { Injectable } from '@angular/core';
+
 import { MenuItem } from './../menu-item/menu-item.model';
 import { CartItem } from './cart-item.model';
+
+import { NotificationService } from './../../shared/messages/notification.service';
+
+@Injectable()
 export class ShoppingCartService {
 
-    items: CartItem[] = []
+  constructor(private notificationService: NotificationService) { }
 
-    limpar() {
-        this.items = []
-    }
+  items: CartItem[] = [];
 
-    adicionarItens(item: MenuItem) {
-        let foundItem = this.items.find((mItem) => mItem.menuItem.id === item.id)
-        if (foundItem) {
-            this.increaseQty(foundItem)
-        } else {
-            this.items.push(new CartItem(item))
-        }
-    }
+  limpar() {
+    this.items = [];
+  }
 
-    removerItem(item: CartItem) {
-        this.items.splice(this.items.indexOf(item), 1)
+  adicionarItens(item: MenuItem) {
+    let foundItem = this.items.find((mItem) => mItem.menuItem.id === item.id)
+    if (foundItem) {
+      this.increaseQty(foundItem);
+    } else {
+      this.items.push(new CartItem(item));
     }
+    this.notificationService.notify(`Novo item adicionado: ${item.name}`);
+  }
 
-    total(): number {
-        return this.items
-            .map(item => item.calcularValor())
-            .reduce((prev, value) => prev + value, 0)
-    }
+  removerItem(item: CartItem) {
+    this.items.splice(this.items.indexOf(item), 1);
+    this.notificationService.notify(`Item: ${item.menuItem.name} removido`);
+  }
 
-    increaseQty(item: CartItem) {
-        item.quantidade = item.quantidade + 1
+  total(): number {
+    return this.items
+      .map(item => item.calcularValor())
+      .reduce((prev, value) => prev + value, 0);
+  }
+
+  increaseQty(item: CartItem) {
+    item.quantidade = item.quantidade + 1;
+  }
+  decreasyQty(item: CartItem) {
+    item.quantidade = item.quantidade - 1;
+    if (item.quantidade === 0) {
+      this.removerItem(item);
     }
-    decreasyQty(item: CartItem) {
-        item.quantidade = item.quantidade - 1
-        if (item.quantidade === 0) {
-            this.removerItem(item)
-        }
-    }
+  }
 
 }
--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,38 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import {trigger, state, style, transition, animate} from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+import { NotificationService } from './../notification.service';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'mt-snackbar',
   templateUrl: './snackbar.component.html',
   styleUrls: ['./snackbar.component.css'],
-  animations:[
-  trigger('snack-visibility',[
-	state('hidden', style({
-		opacity: 0,
-		bottom: '0px'
-	})),
-	state('visibile', style({
-		opacity: 1,
-		bottom: '30px'
-	})),
-	transition('hidden => visibile', animate('500ms 0s ease-in')),
-	transition('visibile => hidden', animate('500ms 0s ease-out'))
-  	])
+  animations: [
+    trigger('snack-visibility', [
+      state('hidden', style({
+        opacity: 0,
+        bottom: '0px'
+      })),
+      state('visibile', style({
+        opacity: 1,
+        bottom: '30px'
+      })),
+      transition('hidden => visibile', animate('500ms 0s ease-in')),
+      transition('visibile => hidden', animate('500ms 0s ease-out'))
+    ])
   ]
 })
 export class SnackbarComponent implements OnInit {
 
   menssagen: string = 'Compras';
-
   snackVisibity = 'hidden';
 
-  constructor() { }
+  constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
+    this.notificationService.notifier
+      .do(message => {
+        this.menssagen = message;
+        this.snackVisibity = 'visibile';
+      }).switchMap(message => Observable.timer(3000))
+      .subscribe(timer => this.snackVisibity = 'hidden');
   }
 
-  teste(){
-  	this.snackVisibity = this.snackVisibity === 'hidden' ? 'visibile' : 'hidden'
+  teste() {
+    this.snackVisibity = this.snackVisibity === 'hidden' ? 'visibile' : 'hidden';
   }
 
 }
