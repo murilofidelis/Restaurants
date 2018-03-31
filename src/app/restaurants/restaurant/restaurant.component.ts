0edaac5857c3f864d2225381c01aaa6bf64d524f
Murilo Fidelis <murilofidelis2012@gmail.com>
2018-03-31 11:03:01 -0300

    animação tela de restaurantes

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,14 +1,25 @@
 import { Restaurant } from './restaurant.model';
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html'
+  templateUrl: './restaurant.component.html',
+  animations: [
+    trigger('exibirRestaurantes', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translate(-30px, -10px)' }),
+        animate('300ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
 
   @Input()
   objRestaurant: Restaurant
+  restauranteEstado = 'ready';
 
   constructor() { }
 
