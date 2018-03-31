6a60bdf252e9bc1dc419af3d5ef33f93d17ce93d
Murilo Fidelis <murilofidelis2012@gmail.com>
2018-03-31 11:24:58 -0300

    animação itens do menu e carrinho de compras

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,16 +1,28 @@
 import { MenuItem } from './menu-item.model';
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('exibiritemMenu', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translateY(-20px)' }),
+        animate('300ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
 
   // representa 1 item do menu
-  @Input() itemDoMenu: MenuItem
+  @Input() itemDoMenu: MenuItem;
   // evento de adicionar
-  @Output() add = new EventEmitter()
+  @Output() add = new EventEmitter();
+
+  menuItemEstado = 'ready';
 
   constructor() { }
 
@@ -18,7 +30,7 @@ export class MenuItemComponent implements OnInit {
   }
   // método para adicionar menu clicado ao evento
   adicionar() {
-    this.add.emit(this.itemDoMenu)
+    this.add.emit(this.itemDoMenu);
   }
 
 }
