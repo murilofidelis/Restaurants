2a0163982d361a09c0a04ad17fed0e78978de537
Murilo Fidelis <murilofidelis2012@gmail.com>
2018-04-14 11:57:26 -0300

    inserindo campo pesquisa de restaurante

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,19 +1,40 @@
 import { RestaurantsService } from './restaurants.service';
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 import { Restaurant } from './restaurant/restaurant.model';
+
 @Component({
   selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+  templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('togglesearch', [
+      state('hidden', style({
+        opacity: 0, 'max-height': '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        'max-height': '70px',
+        'margin-top': '20px'
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurants: Restaurant[]
+  restaurants: Restaurant[];
+
+  searchBarState = 'hidden';
 
   constructor(private restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
-    this.restaurantsService.buscarRestaurantes().subscribe(allRestaurants => this.restaurants = allRestaurants)
+    this.restaurantsService.buscarRestaurantes().subscribe(allRestaurants => this.restaurants = allRestaurants);
+  }
+
+  toggleSearch() {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
   }
 
 }
